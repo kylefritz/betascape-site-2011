e68ae7ae0a6bf63c97d870011c7fe077090f4228
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-08-17 21:52:48 -0400

    put ribbons in

--- js/script.js
+++ js/script.js
@@ -1,26 +1,12 @@
-/* Author: 
+/* Author: Kyle Fritz <kyle.p.fritz@gmail.com>
 
 */
+$(function(){
 
+$('.attendees').eventbrite_attendees({
+  app_key: 'C2TEMDHGVVBC7PVYUN',
+  event_id: '943934333' //hackathon
+  //event_id: '2015326897' //betascape
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
