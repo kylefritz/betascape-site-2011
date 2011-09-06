19b88dd492ad1a2e7da4842dae5ca12c063db0a3
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-09-05 20:14:11 -0400

    reorganize files a little

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+sass_dir = "_sass"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
